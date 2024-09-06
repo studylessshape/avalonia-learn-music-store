8bdc7532d0616235431cd4036e043bbb65b473c8
studylessshape <a664554724@qq.com>
2024-09-06 10:24:17 +0800

    ✨ feat(MusicStoreView): [6] mock search
    finish https://docs.avaloniaui.net/zh-Hans/docs/tutorials/music-store-app/mock-search
    
    from: https://docs.avaloniaui.net/zh-Hans/docs/tutorials/music-store-app/mock-search

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,20 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Collections.ObjectModel;
 
 namespace Avalonia.MusicStore.ViewModels
 {
     public partial class MusicStoreViewModel : ViewModelBase
     {
+        public MusicStoreViewModel()
+        {
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+        }
+
         [ObservableProperty]
-        private AlbumViewModel? _result;
+        private string _searchText;
+        [ObservableProperty]
+        private bool _isBusy;
+
+        [ObservableProperty]
+        private AlbumViewModel? _selectedAlbum;
+
+        public ObservableCollection<AlbumViewModel> SearchResults { get; } = [];
 
         public event Action<AlbumViewModel?>? EnsureSelectEvent;
 
         [RelayCommand]
         private void EnsureSelect()
         {
-            EnsureSelectEvent?.Invoke(Result);
+            EnsureSelectEvent?.Invoke(SelectedAlbum);
         }
     }
 }
